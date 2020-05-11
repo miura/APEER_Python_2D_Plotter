05bf3d965feeca8d698ec8e38ff1ec5cb6a9a6fe
Kota Miura <kotapub@gmail.com>
2020-05-11 18:19:30 +0200

    new script for plotting added

---
+++ plot_2Dtrack.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# Our main plotting package (must have explicit import of submodules)
+import bokeh.io
+import bokeh.plotting
+import holoviews as hv
+from holoviews import opts
+hv.extension('bokeh')
+
+def execute( csvfilepath ):
+    #df = pd.read_csv('Dataset/desktop_tracks.csv')
+    df = pd.read_csv( csvfilepath )
+    print( max(df['Mean']) )
+    tracks = [ df.loc[df['Mean'] == x, ['Slice', 'X', 'Y', 'Mean']] for x in range(1, int(max(df['Mean']))) ]
+    print( "Number of Tracks:", len(tracks))
+    tracklists = [ list(zip(tracks[x]['X'], tracks[x]['Y'], tracks[x]['Slice'])) for x in range( len(tracks) ) ]
+    tracklists[0:3]
+    trackplot = hv.Path(tracklists, vdims='time')
+    trackplot.opts(cmap='Inferno', color='time', line_width=2 )
+    
+    outfilename = "trackplot.html"
+    hv.save( trackplot , outfilename, fmt='html')
+
+    #file_name = "hello_world.txt"
+    #copyfile(input, file_name)
+    #f = open(file_name, "w")
+    #f.write("Hello, World!")
+    #f.close()
+
+    return {'Output': outfilename}
